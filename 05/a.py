9834cee8fd61d730091e323c62f173a6a90ded1e
Patrick Nappa <patricknappa@gmail.com>
2018-12-05 16:32:34 +1100

    day 5

---
+++ 05/a.py
@@ -0,0 +1,25 @@
+
+x = []
+with open('input', 'r') as ifile:
+    for line in ifile.readlines():
+        x = list(line.strip())
+
+def ispolar(a,b):
+    if a is None or b is None:
+        return False
+    if a == b:
+        return False
+    if a.isupper() == b.isupper():
+        return False
+    return abs(ord(b)-ord(a)) == 32
+
+while True:
+    for index in range(len(x)-1):
+        if ispolar(x[index], x[index+1]):
+            x[index] = None
+            x[index+1] = None
+    if x.count(None) == 0:
+        break
+    x = [a for a in x if a is not None]
+
+print(len(x))
