f12bb2156e7b444b6460b24dd5197d5eacef466b
Patrick Nappa <patricknappa@gmail.com>
2018-12-26 15:40:32 +1100

    removed redundant labels

--- 21/decompiled.c
+++ 21/decompiled.c
@@ -8,7 +8,7 @@
 
 //#define WAIT getchar();
 #define WAIT ;
-#define DUMP_REG printf("registers: [%d, %d, _, %d, %d, %d]\n", r0, r1, r3, r4, r5); WAIT
+#define DUMP_REG printf("registers: [%d, %d, _, %d, %d, %d]\n", r0, r1, r2, r3, r5); WAIT
 #define DEBUG_REG ;
 #define DEBUG_REG DUMP_REG
 
@@ -20,68 +20,50 @@ int main() {
     //int r4 = 0; // register 4 is RIP
     int r5 = 0;
 
-instr0:;
-       r3 = 123;
+    r3 = 123;
 instr1:;
-        r3 &= 456;
-instr2:;
-        r3 = r3 == 72;
-instr3:;
+       r3 &= 456;
+       r3 = r3 == 72;
        if (r3 == 1) {
-        goto instr5;
+           puts("bitwise is not broken");
+           goto instr5;
        }
-instr4:;
        // loop in the shitty bitwise routine
        // if 123 & 456 != 72
        goto instr1;
 instr5:;
        // reset register 3 after bitwise test
        r3 = 0;
-instr6:;
        r2 = r3 | 65536;
-instr7:;
        r3 = 1397714;
 instr8:;
        r5 = r2 & 255;
-instr9:;
        r3 += r5;
-instr10:;
-        r3 &= 16777215;
-instr11:;
-        r3 *= 65899;
-instr12:;
-        r3 &= 16777215;
-instr13:;
-        r5 = 256 > r2;
-instr14:;
-        if (r5 == 1) {
-            goto instr16;
-        }
-instr15:;
-        goto instr17;
+       r3 &= 16777215;
+       r3 *= 65899;
+       r3 &= 16777215;
+       r5 = 256 > r2;
+       if (r5 == 1) {
+           goto instr16;
+       }
+       goto instr17;
 instr16:;
         goto instr28;
 instr17:;
         r5 = 0;
 instr18:;
         r1 = r5 + 1;
-instr19:;
         r1 *= 256;
-instr20:;
         r1 = r1 > r2;
-instr21:;
         if (r1 == 1) {
             goto instr23;
         }
-instr22:;
-        // addi 4 1 4
+        // addi 4 1 4 (as instr22)
         goto instr27;
 instr23:;
         // seti 25 2 4
         goto instr26;
-instr24:;
         r5 += 1;
-instr25:;
         // seti 17 0 4
         goto instr18;
 instr26:;
@@ -90,13 +72,13 @@ instr27:;
         goto instr8;
 instr28:;
         r5 = r3 == r0;         
-instr29:;
         // addr 5 4 4
         // go forward 6 instructions!
         // this is the terminating clause
         printf("finished!");
         return 0;
-instr30:;
+
+        // dead code
         // seti 5 8 4
-        goto instr6;
+        // goto instr6;
 }
